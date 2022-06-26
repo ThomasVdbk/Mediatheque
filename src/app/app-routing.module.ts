669cd65521d55e899145c9e0a885deafac2a670b
thomasV <thomaspiece@gmail.com>
2022-06-26 15:11:12 +0200

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundError } from 'rxjs';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [   
+  {path: '', redirectTo: 'home', pathMatch:"full"},
+  {path: "home", component : HomePageComponent},
+  {path:"products", component: ProductsPageComponent},
+  {path:"**", component: NotFoundComponent} // ** si tu ne trouve pas de nom de route parmis les éléments au dessus alors tu passe par là
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
@@ -9,6 +9,13 @@ import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
+import { ProductCardComponent } from './pages/products-page/product-card/product-card.component';
+import { ProductListComponent } from './pages/products-page/product-list/product-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +23,17 @@ import { ProductsPageComponent } from './pages/products-page/products-page.compo
     FooterComponent,
     NotFoundComponent,
     HomePageComponent,
-    ProductsPageComponent
+    ProductsPageComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
